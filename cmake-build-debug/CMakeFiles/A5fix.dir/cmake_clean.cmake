85580991cae3671d8327fc1dba11388fc2b85512
theoleuthardt <theo@maschine.fritz.box>
2023-12-09 01:40:42 +0100

    file read and write node

---
+++ cmake-build-debug/CMakeFiles/A5fix.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "A5fix"
+  "A5fix.pdb"
+  "CMakeFiles/A5fix.dir/aufgabe5_fix.c.o"
+  "CMakeFiles/A5fix.dir/aufgabe5_fix.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A5fix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
